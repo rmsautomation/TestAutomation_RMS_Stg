9b0f45d536e67f332df85233c1f0c48c88662905
Aylin Rodriguez <aylin.rodriguez@magayacatapult.com>
2025-01-15 21:04:20 +0000

    Update cleanCacheAndCookies method

--- Script/CleanCookiesNew.js
+++ Script/CleanCookiesNew.js
@@ -1,5 +1,5 @@
 ﻿
-function Hooks_OnStartTestCase(Sender, StartTestCaseParams)
+function Hooks_OnStartTestCaseOLD(Sender, StartTestCaseParams)
 {
   //Opens the specified URL in a running instance of the specified browser.
   Browsers.Item(btChrome).Navigate(Project.Variables.urlEnvironment);
@@ -13,4 +13,30 @@ function Hooks_OnStartTestCase(Sender, StartTestCaseParams)
   //Closes the 'BrowserWindow' window.
   Aliases.browser.BrowserWindow.Close();
   
+}
+
+function cleanCacheAndCookies() {
+  var browser = Browsers.Item(btChrome);
+  if (browser.Exists) {
+    Aliases.browser.BrowserWindow.Close();
+    Log.Message("Closed any existing Chrome instances.");
+  }
+  var options = "--disable-cache --clear-token-service --disable-application-cache";
+  browser.RunOptions = options;
+  browser.Run();
+  Log.Message("Chrome launched with cache and cookies cleared.");
+  browser.Navigate("about:blank");
+  Log.Message("Navigated to 'about:blank' to complete the cache and cookies cleaning.");
+  Aliases.browser.BrowserWindow.Close();
+  Log.Message("Browser closed after cleaning cache and cookies.");
+}
+ 
+function Hooks_OnStartTestCase(Sender, StartTestCaseParams) {
+
+  cleanCacheAndCookies();
+  var browser = Browsers.Item(btChrome);
+  browser.Run();
+  Log.Message("Chrome launched for testing.");
+  browser.Navigate(Project.Variables.urlEnvironment);
+  Log.Message("Navigated to the specified environment URL: " + Project.Variables.urlEnvironment);
 }
